dafb208ab8c787539d587218126c6fe36fba86c6
ssori0421 <ssori0421@gmail.com>
2022-11-23 09:13:28 +0900

    props 사용해보기

--- src/App.js
+++ src/App.js
@@ -1,31 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
-  // 헤더 컴포넌트
- function Header(){
-   return <header>
-     <h1><a href="/">React  </a></h1>
-   </header>
- } 
- // 아티클 컴포넌트 
- function Article(){
-  return <article>
-            <h2>Welcome</h2>
-            Hello, WEB
-          </article>
-} 
+// 헤더 컴포넌트
+function Header(props) {
+  console.log('props', props, props.title);
+  return (
+    <header>
+      <h1>
+        <a href='/'>{props.title}</a>
+      </h1>
+    </header>
+  );
+}
+//네비게이션 컴포넌트
+function Nav(props) {
+  const lis = [];
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    console.log(t);
+    lis.push(
+      <li key={t.id}>
+        <a href={'/read/' + t.id}>{t.title}</a>
+      </li>
+    );
+  }
+  return (
+    <nav>
+      <ol>{lis}</ol>
+    </nav>
+  );
+}
+// 아티클 컴포넌트
+function Article(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  );
+}
 
 function App() {
+  const topics = [
+    { id: 1, title: 'html', body: 'html is ...' },
+    { id: 2, title: 'css', body: 'css is ...' },
+    { id: 3, title: 'javascript', body: 'javascript is ...' },
+  ];
   return (
     <div>
-       <Header></Header>
-       <nav>
-         <ol>
-           <li><a href="/read/1">html</a></li>
-           <li><a href="/read/2">css</a></li>
-           <li><a href="/read/3">js</a></li>
-         </ol>
-       </nav>
-       <Article></Article>
+      <Header title='WEB'></Header>
+      <Nav topics={topics}></Nav>
+      <Article title='welcome' body='Hello, WEB'></Article>
     </div>
   );
 }
